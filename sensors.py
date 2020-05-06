16a2970e5b80560100ab504e569be726336ff20b
Narin Okazaki <pt.narin.okazaki@magik-eye.com>
2020-05-06 17:32:35 +0900

    added if __name__ == 'main'

--- sensors.py
+++ sensors.py
@@ -1,11 +1,18 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
+import sys
 
 import sensorwindow_ui
 
-
-class SensorWindow(QtWidgets.QWidget):   # 20200325remote https://teratail.com/questions/118024
+class SensorWindow(QtWidgets.QWidget):  # https://teratail.com/questions/118024
     def __init__(self, parent=None):
         super(SensorWindow, self).__init__(parent)
 
         self.ui_s = sensorwindow_ui.Ui_sensor()
         self.ui_s.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    sensorWindow = SensorWindow()
+    sensorWindow.show()
+    app.exec_()
