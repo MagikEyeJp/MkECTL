8161e560399e80ce3f69ff58684a3ef636b08584
Narin Okazaki <pt.narin.okazaki@magik-eye.com>
2020-05-21 16:41:02 +0900

    modified and added

--- sensors.py
+++ sensors.py
@@ -1,8 +1,22 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 import sys
+import re
+
+# from mke-sdk
+import pymkecli.client
+import pymkecli.bus
 
 import sensorwindow_ui
 import ImageViewScene
+
+
+# xのあるbit位置が0か1か調べる
+def getbit(x, b):
+    return (x >> b) & 1
+
+# xのある位置のbitを0または1にした値を返す(vは0または1)
+def setbit(x, b, v):
+    return x & ~(1 << b) | (v << b)
 
 class SensorWindow(QtWidgets.QWidget):  # https://teratail.com/questions/118024
     def __init__(self, parent=None):
@@ -12,12 +26,15 @@ class SensorWindow(QtWidgets.QWidget):  # https://teratail.com/questions/118024
         self.ui_s.setupUi(self)
 
         # Variables (initialized with default values)
-        self.IPaddress = '192.168.10.123:8888'
+        self.IPaddress = '192.168.100.140'  # dummy
+        self.portNum: int = 8888
+        self.RPiaddress = self.IPaddress + ':' + str(self.portNum)
         self.shutterSpeed: int = 30000
         self.frames: int = 5
         self.ISOvalue: int = 400    # value in the list of combo box
         self.hexLaserPattern: hex = 0x0000    # Hex 4 digits
         self.binLaserPattern: bin = bin(self.hexLaserPattern)
+        self.decLaserPattern: int = int(self.hexLaserPattern)
 
         # Combo Box
         self.ui_s.ISOcombo.setCurrentText(str(self.ISOvalue))
@@ -27,8 +44,8 @@ class SensorWindow(QtWidgets.QWidget):  # https://teratail.com/questions/118024
         self.ui_s.hex4dCheckBox.stateChanged.connect(self.laser_custom)
 
         # Line edit textChanged
-        self.ui_s.IPlineEdit.setText(self.IPaddress)
-        self.ui_s.IPlineEdit.textChanged.connect(self.changeIPaddress)
+        self.ui_s.IPlineEdit.setText(self.RPiaddress)
+        # self.ui_s.IPlineEdit.textChanged.connect(self.changeIPaddress)
         self.ui_s.shutterLineEdit.setText(str(self.shutterSpeed))
         self.ui_s.shutterLineEdit.textChanged.connect(self.changeShutter)
         self.ui_s.framesLineEdit.setText(str(self.frames))
@@ -36,6 +53,7 @@ class SensorWindow(QtWidgets.QWidget):  # https://teratail.com/questions/118024
         self.ui_s.ISOlineEdit.textChanged.connect(lambda: self.changeISO('lineEdit'))
 
         # Line edit returnPressed
+        self.ui_s.IPlineEdit.returnPressed.connect(self.changeIPaddress)
         self.ui_s.hex4dLineEdit.returnPressed.connect\
             (lambda: self.setLaser('0x' + self.ui_s.hex4dLineEdit.text()))
 
@@ -47,12 +65,17 @@ class SensorWindow(QtWidgets.QWidget):  # https://teratail.com/questions/118024
 
         # Push buttons
         self.ui_s.reconnectButton.clicked.connect(self.connectToSensor)
+        self.ui_s.setIPaddressButton.clicked.connect(self.changeIPaddress)
+        self.ui_s.evenLaserButton.clicked.connect(lambda: self.setLaser('0xAAAA'))
+        self.ui_s.oddLaserButton.clicked.connect(lambda: self.setLaser('0x5555'))
+        self.ui_s.onAllLaserButton.clicked.connect(lambda: self.setLaser('0xFFFF'))
+        self.ui_s.offAllLaserButton.clicked.connect(lambda: self.setLaser('0x0000'))
         self.ui_s.setHex4dLaserButton.clicked.connect\
             (lambda: self.setLaser('0x' + self.ui_s.hex4dLineEdit.text()))
 
         # Label
         # self.ui_s.CurrentLaserPattern_value.setText(str(format(0, '016b')))
-        self.ui_s.CurrentLaserPattern_value.setText(self.binLaserPattern.replace('0b', '').zfill(16))
+        self.ui_s.CurrentLaserPattern_value.setText('-'.join(self.binLaserPattern.replace('0b', '').zfill(16)))
 
         # tooltip
         self.ui_s.ISOlineEdit.setToolTip('select \"Custom\" and set integer here')
@@ -61,8 +84,17 @@ class SensorWindow(QtWidgets.QWidget):  # https://teratail.com/questions/118024
         self.connectToSensor()
 
     def changeIPaddress(self):
-        self.IPaddress = self.ui_s.IPlineEdit.text()
-        # print(self.IPaddress)
+        self.RPiaddress = self.ui_s.IPlineEdit.text()
+        if ':' in self.RPiaddress:
+            # https://teratail.com/questions/61914
+            pattern = "(.*):(.*)"
+            d = re.search(pattern, self.RPiaddress)
+            self.IPaddress = d.group(1)
+            self.portNum = int(d.group(2))
+            print(1)
+        else:
+            self.IPaddress = self.RPiaddress
+            print(2)
 
     def changeShutter(self):
         if self.ui_s.shutterLineEdit.text() == "":
@@ -121,15 +153,20 @@ class SensorWindow(QtWidgets.QWidget):  # https://teratail.com/questions/118024
             self.ui_s.setHex4dLaserButton.setEnabled(False)
 
     def setLaser(self, hex4d):
-        if len(hex4d) != 4:
+        if len(hex4d) != 2 + 4:
             QtWidgets.QMessageBox.critical(self, "Hex 4 digits", "Please type a 4-digit number in base 16 system.")
         else:
-            # temp
+            # https://stackoverflow.com/questions/21879454/how-to-convert-a-hex-string-to-hex-number
             self.hexLaserPattern = hex(int(hex4d, 16))
-            # print(self.hexLaserPattern)
-            # print(type(self.hexLaserPattern))
             self.binLaserPattern = bin(int(hex4d, 16))
-            self.ui_s.CurrentLaserPattern_value.setText(self.binLaserPattern.replace('0b', '').zfill(16))
+            self.decLaserPattern = int(hex4d, 16)
+            laserpattern_print = self.binLaserPattern.replace('0b', '').zfill(16)
+            self.ui_s.CurrentLaserPattern_value.setText('-'.join(laserpattern_print[::-1]))
+            # https://qiita.com/Hawk84/items/ecd0c7239e490ea22308   https://note.nkmk.me/python-string-concat/
+
+            for i in range(16):
+                # print(getbit(self.decLaserPattern, i))
+                print(setbit(self.decLaserPattern, i, int(laserpattern_print[15-i])))
 
 
 
