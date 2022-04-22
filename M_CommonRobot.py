3c7f87336759a3fd5aa36d168a7f3e180a633545
uribo <ur1b0.dev@gmail.com>
2022-04-22 17:02:54 +0900

    add stub methods

---
+++ M_CommonRobot.py
@@ -0,0 +1,36 @@
+from IMotorRobot import IMotorRobot
+
+class RobotIF(IMotorRobot):
+    def __init__(self, machineParams_m=None):
+        self.params = None
+
+    def getMotorDic(self):
+        self.params = {
+            'slider': {
+                'id': 'x',
+                'cont': "Controller object",
+                'scale': 0,
+                'SN': 'xxxxxxxxxxx'
+            },
+            'pan': {
+                'id': 'pan',
+                'cont': "Controller object",
+                'scale': 0,
+                'SN': 'xxxxxxxxxxx'
+            },
+            'tilt': {
+                'id': 'tilt',
+                'cont': "Controller object",
+                'scale': 0,
+                'SN': 'xxxxxxxxxxx'
+            }
+        }
+
+    def initializeMotors(self):
+        return True
+
+    def changePIDparam(self, pid_category, pid_param, motor_i, value):
+        pass
+
+    def goToTargetPos(self, targetPos, callback, wait=False, isAborted=None, scriptParams=None, mainWindow=None):
+        True
